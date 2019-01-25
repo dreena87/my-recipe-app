f155dd5f3a981de8b2292ce38f458801a41273df
Adreena Howard <adreena.howard@yahoo.com>
2019-01-25 13:35:54 -0600

    fixed picture size

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Form from "./components/Form";
+import Recipes from "./components/Recipes";
 
 const API_KEY = "22988db4db7ff4547a1534306215b6e0";
 
@@ -31,14 +32,7 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
-        {this.state.recipes.map(recipe => {
-          return (
-            <div>
-              <img src={recipe.image_url} alt={recipe.title}/>
-              <p key={recipe.recipe_id}>{recipe.title}</p>
-            </div>
-          );
-        })}
+        <Recipes/>
       </div>
     );
   }
