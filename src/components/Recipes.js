40854c014f3517b3c8f56b8c88d26bbaa6a470f6
Adreena Howard <adreena.howard@yahoo.com>
2019-01-25 14:15:41 -0600

    added titles

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -7,15 +7,15 @@ const Recipes = props => (
     <div className="row">
       {props.recipes.map(recipe => {
         return (
-          <div className="col-md-4">
+          <div key={recipe.title} className="col-md-4" style={{ marginBottom:"2rem" }}>
             <div className="recipe__box">
               <img
                 className="recipe__box-img"
-                src={rec.img_url}
-                alt={recipe.title}
-              />
+                src={recipe.image_url}
+                alt={recipe.title}/>
               <div className="recipe__text">
-              
+              <h5 className="recipes__title">{recipe.title}</h5>
+              <p className="recipes__subtitle"></p>
               </div>
             </div>
           </div>
