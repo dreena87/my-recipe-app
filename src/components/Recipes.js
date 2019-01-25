4955bbf93f94c60421d7a1570790a98ffa893507
Adreena Howard <adreena.howard@yahoo.com>
2019-01-25 13:46:00 -0600

    added props elements

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 import { Link } from "react-router-dom";
 
-const Recipes = () => (
+const Recipes = props => (
  <div> 
-   Recipes!
+   {props.recipes.map(recipe => {
+  return (
+    <div key={recipe.recipe_id}>
+      <img src={recipe.image_url} alt={recipe.title} />
+      <p>{recipe.title}</p>
+    </div>
+  );
+})}
  </div>
 );
 
